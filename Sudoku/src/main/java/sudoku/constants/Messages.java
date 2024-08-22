e288341fa2ca6116defc349eb697f79bfebc0b48
Lesa Hettiarachchi <118477767+lesa-hettiarachchi@users.noreply.github.com>
2024-08-23 00:07:55 +1000

    Delete Sudoku directory

--- Sudoku/src/main/java/sudoku/constants/Messages.java
+++
@@ -1,6 +0,0 @@
-package sudoku.constants;
-
-public class Messages {
-    public static final String  GAME_COMPLETE= "Congratulations, You have won the game!! New Game? ";
-    public static final String  Error = "An Error has occurred. Please restart the game.";
-}
